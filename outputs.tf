5a3c63ceb50649aa9cc29a3ddb2bcb75d231eda0
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-10 05:46:38 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,6 @@ output "region" {
   
   value = {
     name              = local.region.name
-    provider          = provider.vdc
     vdc               = local.region.vdc
     edge              = local.region.edge
   }
