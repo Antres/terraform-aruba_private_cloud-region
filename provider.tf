87f03adcb874cb40c778efa26f78bef53f1680ac
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-10 05:45:16 +0200

    Delete provider.tf

--- provider.tf
+++
@@ -1,8 +0,0 @@
-provider "vcd" {
-  
-  user                    = var.username
-  password                = var.password
-  org                     = var.organization
-  
-  url                     = "https://admin01.dc${var.cell_id}.private.cloud.it/api"
-}
